9b13bf83963d0579cc0334ebfcc498810464883e
선상원 <45645824+SeonSangWon@users.noreply.github.com>
2019-06-02 17:46:16 +0900

    Update Jython.java

--- Jython.java
+++ Jython.java
@@ -3,6 +3,13 @@ package connection;
 public class Jython {
 
 	public static void main(String[] args) {
+		//Upper_Tensorflow를 통해 전신사진을 상의영역만 자른 후, 추론
+		//Lower_Tensorflow를 통해 전신사진을 하의영역만 자른 후, 추론
+		//restore를 통해 이미지를 복구한다.
+		//이미지를 추론하는 과정에서 이미지의 경로를 지정해주어야하기 때문에
+		//upper.py 를 이용해 상의 영역을 자른 후, 이미지를 덮어씌워 분석시킨다.
+		//lower.py 를 이용해 하의 영역을 자른 후, 이미지를 덮어씌워 분석시킨다.
+		
 		Tensorflow tf = new Tensorflow();
 		
 		String image = "C:\\Users\\sangw\\OneDrive\\Desktop\\data\\5.jpg";
